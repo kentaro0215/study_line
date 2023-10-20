9e39caaac1182a50b5e599a0607ad9cc89512931
kentaro0215 <ken.runteq0215@gmail.com>
2023-10-21 03:10:25 +0900

    first commit

---
+++ lib/study_line.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "study_line/version"
+
+module StudyLine
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/study_line/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StudyLine
+  VERSION = "0.1.0"
+end
